ae665584e61e51aa26740c57939af873738ed846
trodelgal <67858041+trodelgal@users.noreply.github.com>
2020-10-06 00:22:25 +0300

    try to start! first test done

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,43 +1,71 @@
 import React, {useState,useEffect} from 'react';
 import Modal from '@material-ui/core/Modal';
-
+import Square from './Square';
 function Board() {
     const [board, setBoard] = useState([[]]); 
     const [winner, setWinner] = useState();
-
+    const [currentPlayer,setCurrentPlayer] = useState('player1');
+    // const [value,setValue] = useState('player1');
+ 
     function emptyBoard(){
         const gameBoard = [];
         for(let i = 0; i < 7; i++){
             gameBoard[i] = [];
-/*           for(Your Code Here){
-                // Your Code Here
-            }  */
+            for(let j = 0; j < 6; j++){
+                gameBoard[i][j] = [];
+            }
         };
         setBoard(gameBoard);
         setWinner(false);
     }
-
     useEffect(() => {
         emptyBoard();
     }, [])
 
+    function handleClick(i){
+        console.log(i);
+        const newBoard = board.slice();
+        console.log(newBoard[i]);
+        newBoard[i]
+        // let column = e.target.id.slice(6)
+        // const gameBoard = board
+        // console.log(gameBoard[column]);
+        // for(let i=0; i<6;i++){
+        //     console.log(gameBoard[column][i],i);
+        //     if(){
+        //         gameBoard[column][i]=currentPlayer 
+        //         break;
+        //     }
+        // }
+        currentPlayer === 'player1'? setCurrentPlayer('player2'):setCurrentPlayer('player1');
+        // gameBoard[column][index][0]=value;
+        // setBoard(gameBoard)
+        // blueIsNext? setValue('player2'): setValue('player1');
+        // setBlueIsNext(lastValue=> !lastValue)
+    }
   return (
       <>
     <div className="Board">
         {
             board.map((column, i) => {
-                return <>
+                return( 
+                <>
                 {i===0&& <div className="divider" />} {/* the red column separating each column */}
-                <div className="Column" id={`column${i}`}>
-                <span>{i}</span> {/* should remove once you understand the concept */}
+                <div className="Column" id={`column${i}`} onClick={()=>handleClick(i)}>
+                {column[0]}
                 {
-                    /* 
-                        here you should have your squares implemented, each with a className 'Square' and an id of - square0 -> square1 -> square2
-                    */
+                    
+                    column.map((value,i)=>{
+                        return(
+                           <div className='square' id={`square${i}`}>
+                           </div>
+                        )
+                    })
                 }
                 </div>
                 <div className="divider" /> {/* the red column separating each column */}
                 </>
+                )
             })
         }
     </div>
---
+++ src/components/Square.js
@@ -0,0 +1,11 @@
+import React, {useState,useEffect} from 'react';
+
+function Square({ id}){
+
+    return (
+      <div>
+      </div>
+    );
+  }
+
+export default Square;
